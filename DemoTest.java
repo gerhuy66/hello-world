b29b11a4dca0d7660f87aaa5a18ab2226415dc49
lcdhuy <duchuy1096@gmail.com>
2019-12-30 22:29:48 +0700

    update 30/12/2019

--- DemoTest.java
+++ DemoTest.java
@@ -1,5 +1,7 @@
 import java.util.Random; 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 public class DemoTest
 {
 
@@ -7,6 +9,7 @@ public class DemoTest
 
     public static void main(String[] args)
     {
+        /*
         Random rand = new Random();
         int[] arr = new int[10];
         
@@ -25,6 +28,30 @@ public class DemoTest
         for(int i = 0; i <10; i++){
             System.out.print(arr[i]);
             System.out.print(" ");
+        }
+        */
+        Calendar cal = Calendar.getInstance();
+        Date date = new Date();
+        boolean running = true;
+        int tmp = cal.get(Calendar.MINUTE);
+        while(running)
+        {
+            cal = Calendar.getInstance();
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException ex)
+            {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Hour:"+cal.get(Calendar.HOUR));
+            System.out.println("Minute:"+cal.get(Calendar.MINUTE));
+            System.out.println("Seccond:"+cal.get(Calendar.SECOND));
+            if(cal.get(Calendar.MINUTE) > tmp)
+            {
+                running= false;
+            }
         }
     }
 }
